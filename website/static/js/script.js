2242585147354c1552d043518855a0bb04614867
Ankit Chhetri <64682052+Ankitchh@users.noreply.github.com>
2024-03-26 20:23:57 +0000

    new minor updates

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -7,59 +7,38 @@ const locoScroll = new LocomotiveScroll({
 el: document.querySelector("#main"),
 smooth: true
 });
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
-
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#main", {
-scrollTop(value) {
-  return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-}, // we don't have to define a scrollLeft because we're only scrolling vertically.
-getBoundingClientRect() {
-  return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-},
-// LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-});
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
 }
-loco()
+l  oco()
 
-
-document.querySelectorAll("#blog").forEach(function(elem){
-
-  var rotate = 0;
-  var r_diff = 0;
-
-  elem.addEventListener("mouseleave", function(){
-      gsap.to(elem.querySelector("img"),{
-          opacity: 0,
-          ease: Power3,
-      });
-  });
-
+function page1aAni(){
+  var tl = gsap.timeline();
 
  
+    tl.from(".barmenu",{
+        y: '-10%',
+        opacity: 0,
+        duration: .5,
+        ease: Expo.easeInOut
+    })
+    tl.from("form",{
+        y: '-10%',
+        opacity: 0,
+        duration: 1,
+        ease: Expo.easeInOut
+    })
+
+    .to(".aniElem",{
+      y: 0,
+      duration: 2,
+      delay:-1,
+      ease: Expo.easeInOut,
+      stagger:.2
+  })
+}
+
+page1aAni();
 
 
-  elem.addEventListener("mousemove", function(details){
-      var diff = details.clientY - elem.getBoundingClientRect().top;
-      r_diff = details.clientX - rotate;
-      rotate = details.clientX;
-      gsap.to(elem.querySelector("img"),{
-          opacity: 1,
-          ease: Power3,
-          top: diff,
-          left: details.clientX,
-          rotate: gsap.utils.clamp(-20,20,r_diff)
-      });
-  });
-});
 
 
 
